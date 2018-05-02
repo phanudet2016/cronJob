21ef4508a2159f369412be06b01cbb4e26b7c072
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2018-05-02 21:18:10 +0700

    commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+var nodemailer = require('nodemailer')
 
 const app = express()
 app.use(morgan('combined'))
@@ -15,6 +16,38 @@ app.get('/posts', (req, res) => {
       description: "Hi there! How are you?"
     }]
   )
+  sendEmail()
   console.log('hello cron')
 })
+
+function sendEmail() {
+  // sendEmail
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    port: 25,
+    auth: {
+      user: '5706021622141@fitm.kmutnb.ac.th',
+      pass: '08486787bg'
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+  
+  let HelperOptions = {
+    from: '"Beer" <5706021622141@fitm.kmutnb.ac.th>',
+    to: 'panudach_beer_2012@hotmail.co.th',
+    subject: '555',
+    text: 'GGGG'
+  };
+  
+  transporter.sendMail(HelperOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("The message was sent!");
+      console.log(info);
+  })
+}
 app.listen(process.env.PORT || 8081)
